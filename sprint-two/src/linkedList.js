a253ddf793439ea18d531f1ce2e4a78ed0fe92ac
Emanuel <manny.cov09@gmail.com>
2018-08-08 21:21:24 -0700

    complete linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,44 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node;
+    if (value) {
+      node = new Node(value);
+      if (list.head === null && list.tail === null) {
+        list.head = node;
+        list.tail = node;
+      } else {
+        list.head.next = node;
+        list.tail = node;
+      }
+    }
   };
 
   list.removeHead = function() {
+    var oldHead;
+
+    if (list.head) {
+      oldHead = list.head.value;
+      list.head = list.head.next;
+    }
+    return oldHead;
   };
 
   list.contains = function(target) {
+    var node = list.head.next;
+    if (target) {
+      while (node !== null) {
+        if (list.head.value === target) {
+          return true;
+        } else if (node.value === target) {
+          return true;
+        } else {
+          node = node.next;
+        }
+      }
+      return false;
+    }
   };
-
   return list;
 };
 
